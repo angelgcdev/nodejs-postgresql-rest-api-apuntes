8df91fa88f3834a19cf41e7267d8c7e5c7a47380
Luis Gutierrez <angelgcdev@gmail.com>
2024-10-13 18:55:27 -0400

    first commit

---
+++ database/db.sql
@@ -0,0 +1,17 @@
+\ l;
+CREATE DATABASE nodepg;
+\ c;
+--;
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+--;
+INSERT INTO users(name, email)
+VALUES ('John Doe', 'john@gmail.com'),
+  ('Jane Doe', 'jane@gmail.com');
+--;
+SELECT *
+FROM users;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT } from "./config.js";
+import { router } from "./routes/users.routes.js";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT);
+console.log("Server on port", PORT);
